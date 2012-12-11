b976477f4467053ab593b26c72678f8360032442
ahawker <andrew.r.hawker@gmail.com>
2012-12-11 00:02:56 -0800

    Added support for tuples, nil and strings. Stubbed in the remaining decode functions.

--- decoding.py
+++ decoding.py
@@ -51,12 +51,57 @@ class ETFDecoder(object):
     def decode_small_tuple(self, data, pos):
         offset = pos + 1
         arity = struct.unpack(format.INT8, data[pos:offset])[0]
-        x = lambda d,p: ((d,p) for (d,p) in self.decode_term(d,p) for _ in range(0, arity))
-        #z = lambda c: ((d,p) for (d,p) in x for _ in range(0, c) )
-        #y = ((d,p) for (d,p) in )
-        #x = list(term for (term, pos) in (self.decode_term(data, pos) ))
-        x = []
-        for i in range(0, arity):
-            data, pos = self.decode_term(data, pos)
-            x.append(data)
-        return data, pos
+        return self._decode_iterable(data[:offset], pos, arity, tuple)
+
+    def decode_large_tuple(self, data, pos):
+        offset = pos + 4
+        arity = struct.unpack(format.UINT32, data[pos:offset])[0]
+        return self._decode_iterable(data[:offset], pos, arity, tuple)
+
+    def decode_nil(self, data, pos):
+        return [] # Can we return None here instead ??? TODO
+
+    def decode_string(self, data, pos):
+        offset = pos + 2
+        length = struct.unpack(format.UINT16, data[pos:offset])[0]
+        return [struct.unpack(format.INT8, i) for i in data[offset:offset+length]]
+
+    def decode_list(self, data, pos):
+        pass
+
+    def decode_binary(self, data, pos):
+        pass
+
+    def decode_small_big(self, data, pos):
+        pass
+
+    def decode_large_big(self, data, pos):
+        pass
+
+    def decode_new_reference(self, data, pos):
+        pass
+
+    def decode_small_atom(self, data, pos):
+        pass
+
+    def decode_fun(self, data, pos):
+        pass
+
+    def decode_new_fun(self, data, pos):
+        pass
+
+    def decode_export(self, data, pos):
+        pass
+
+    def decode_bit_binary(self, data, pos):
+        pass
+
+    def decode_new_float(self, data, pos):
+        pass
+
+    def _decode_iterable(self, data, pos, length, type):
+        def _decoded_term_generator(data, pos, length):
+            for _ in range(0, length):
+                data, pos = self.decode_term(data, pos)
+                yield data
+        return type(_decoded_term_generator(data, pos, length))
