23a3e3b0f776cb7264af5594459783264ba2c60d
ahawker <andrew.r.hawker@gmail.com>
2012-12-12 19:27:43 -0800

    Stubbed in encode_* functions. Fleshed them all out but i'd assume some will have be to merged based on type.

--- encoding.py
+++ encoding.py
@@ -1,14 +1,87 @@
+import format
+import struct
+import tags
+import terms
+import zlib
+
 __author__ = 'ahawker'
 
+class ETFEncodingError(Exception):
+    pass
+
 class ETFEncoder(object):
-    def __init__(self, encoding='utf-8'):
+    def __init__(self):
         super(ETFEncoder, self).__init__()
-        self.encoding = encoding
 
     def encode(self, data):
         pass
 
-class ETFCodec(object):
+    def encode_term(self, data):
+        pass
+
+    def encode_compressed_term(self, data):
+        pass
+
+    def encode_small_integer(self, data):
+        pass
+
+    def encode_integer(self, data):
+        pass
+
+    def encode_float(self, data):
+        pass
+
+    def encode_atom(self, data):
+        pass
+
+    def encode_reference(self, data):
+        pass
+
+    def encode_small_tuple(self, data):
+        pass
+
+    def encode_large_tuple(self, data):
+        pass
+
+    def encode_nil(self, data):
+        pass
+
+    def encode_string(self, data):
+        pass
+
+    def encode_list(self, data):
+        pass
+
+    def encode_binary(self, data):
+        pass
+
+    def encode_small_big(self, data):
+        pass
+
+    def encode_large_big(self, data):
+        pass
+
+    def encode_new_reference(self, data):
+        pass
+
+    def encode_small_atom(self, data):
+        pass
+
+    def encode_fun(self, data):
+        pass
+
+    def encode_new_fun(self, data):
+        pass
+
+    def encode_export(self, data):
+        pass
+
+    def encode_bit_binary(self, data):
+        pass
+
+    def encode_new_float(self, data):
+        pass
+
+if __name__ == '__main__':
+    e = ETFEncoder()
     pass
-#codec contains both encoding and decoding
-#codec is created in __init__
