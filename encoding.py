9f40fe024b294d4ac2e9b74b6268627ecad637f5
ahawker <andrew.r.hawker@gmail.com>
2012-12-12 19:58:20 -0800

    Added types deco and stubbed in encoding_* funcs.

--- encoding.py
+++ encoding.py
@@ -1,3 +1,4 @@
+import functools
 import format
 import struct
 import tags
@@ -5,6 +6,18 @@ import terms
 import zlib
 
 __author__ = 'ahawker'
+__all__ = ['ETFEncodingError', 'ETFEncoder']
+
+def types(*etypes):
+    def decorator(func):
+        @functools.wraps(func)
+        def f(*args, **kwargs):
+            if not isinstance(args[1], etypes):
+                raise ValueError('{0} got type {1} but expects {1}'.format(func.__name__, type(args[1]), etypes))
+            return func(*args, **kwargs)
+        f.types = etypes
+        return f
+    return decorator
 
 class ETFEncodingError(Exception):
     pass
@@ -22,50 +35,65 @@ class ETFEncoder(object):
     def encode_compressed_term(self, data):
         pass
 
-    def encode_small_integer(self, data):
-        pass
+#    def encode_small_integer(self, data):
+#        pass
 
+    @types(int)
     def encode_integer(self, data):
         pass
 
+    @types(float)
     def encode_float(self, data):
         pass
 
-    def encode_atom(self, data):
+    @types(bool, type(None), type(terms.Atom))
+    def encode_atom(self, data): #& small atom
         pass
 
+    @types(type(terms.Reference))
     def encode_reference(self, data):
         pass
 
-    def encode_small_tuple(self, data):
+#    def encode_small_tuple(self, data):
+#        pass
+#
+#    def encode_large_tuple(self, data):
+#        pass
+
+    @types(tuple)
+    def encode_tuple(self, data): #small/large tuple
         pass
 
-    def encode_large_tuple(self, data):
+#    def encode_nil(self, data):
+#        pass
+
+    @types(str, unicode)
+    def encode_string(self, data): #& binary
         pass
 
-    def encode_nil(self, data):
+    @types(list)
+    def encode_list(self, data): #& NIL
         pass
 
-    def encode_string(self, data):
+#    def encode_binary(self, data):
+#        pass
+
+#    def encode_small_big(self, data):
+#        pass
+#
+#    def encode_large_big(self, data):
+#        pass
+
+    @types(long)
+    def encode_big(self, data):#small/big
         pass
 
-    def encode_list(self, data):
-        pass
-
-    def encode_binary(self, data):
-        pass
-
-    def encode_small_big(self, data):
-        pass
-
-    def encode_large_big(self, data):
-        pass
-
+    @types(type(terms.Reference))
     def encode_new_reference(self, data):
         pass
 
-    def encode_small_atom(self, data):
-        pass
+#    def encode_small_atom(self, data):
+#        pass
 
     def encode_fun(self, data):
         pass
@@ -73,6 +101,7 @@ class ETFEncoder(object):
     def encode_new_fun(self, data):
         pass
 
+    @types(type(terms.Export))
     def encode_export(self, data):
         pass
 
